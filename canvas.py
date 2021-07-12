c45ab6702480b09e60d47049b76d2b9785a967bd
Tomson0119 <kjs980119@gmail.com>
2021-07-12 22:22:37 +0900

    움직이는 타켓 블록을 따로 설정한다.

--- canvas.py
+++ canvas.py
@@ -1,13 +1,15 @@
 import tkinter as tk
+from util import *
 
 
 class MyCanvas:
-    def __init__(self, master, size, block_size, border=1):
+    def __init__(self, master, size, block_size, bd=1):
         self.tw, self.th = size
         self.bw, self.bh = block_size
         self.rows, self.cols = 0, 0
 
-        self.canvas = tk.Canvas(master, width=self.tw, height=self.th, bg='white', relief='solid', bd=border)
+        self.canvas = tk.Canvas(master, width=self.tw, height=self.th, bg='white', relief='solid', bd=bd)
+        self.target = []
 
     def place(self, px, py):
         self.canvas.place(x=px, y=py)
@@ -20,17 +22,17 @@ class MyCanvas:
         for j in range(1, cols):
             self.canvas.create_line(self.bw*j, 2, self.bw*j, self.th+2, fill='light gray')
 
-    def draw_block(self, block):
-        color, p = block.color, block.get_pos()
-        for pp in p:
-            x, y = MyCanvas.calc_coord(self.bw, self.bh, pp[0], pp[1])
-            nx, ny = MyCanvas.calc_coord(self.bw, self.bh, pp[0] + 1, pp[1] + 1)
-            self.canvas.create_rectangle(x, y, nx, ny, fill=color)
+    def draw_block(self, pos, color):
+        self.target.clear()
+        for pp in pos:
+            x, y = calc_coord(self.bw, self.bh, pp[0], pp[1])
+            nx, ny = calc_coord(self.bw, self.bh, pp[0] + 1, pp[1] + 1)
+            self.target.append(self.canvas.create_rectangle(x, y, nx, ny, fill=color))
+
+    def move(self, x, y):
+        for block in self.target:
+            self.canvas.move(block, x, y)
 
     def clear(self):
         self.canvas.delete('all')
         self.draw_grid(self.rows, self.cols)
-
-    @staticmethod
-    def calc_coord(w, h, r, c):
-        return w * c, h * r
