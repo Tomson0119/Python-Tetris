cf97eb318ee37f69662dfb885a6155ed94f83554
Tomson0119 <kjs980119@gmail.com>
2021-07-12 22:21:58 +0900

    board에서 캔버스에 그려지는 블록을 다루는 함수들 추가

--- logic.py
+++ logic.py
@@ -16,13 +16,14 @@ class Block:
     def __init__(self):
         self.shape = Block.SHAPE[random.randint(1, 7)]
 
-    def get_pos(self):
+    @property
+    def pos(self):
         ret = []
         lst = self.shape
         for i in range(len(lst)-1):
             row = lst[i] // 4
             col = lst[i] % 4
-            ret.append((row, col))
+            ret.append([row, col])
         return ret
 
     @property
@@ -31,10 +32,49 @@ class Block:
 
 
 class Board:
-    def __init__(self, width, height):
+    def __init__(self, master, size, block_size, width, height):
         self.width = width
         self.height = height
-        self.board = [[0]*width for _ in range(height)]
+        self.bw, self.bh = block_size
+
+        self.dx = 0
+        self.dy = 0
+
+        self.board = [[0]*width for _ in range(height + 4)]
+        self.canvas = MyCanvas(master, size, block_size, 2)
+        self.canvas.draw_grid(height, width)
+
+    def place(self, x, y):
+        self.canvas.place(x, y)
+
+    def insert(self, block):
+        pos = move_coord(block.pos, -2, 3)
+        self.canvas.draw_block(pos, block.color)
+
+    def accumulate_delta(self, dx, dy):
+        self.dx += dx
+        self.dy += dy
+        move_x, move_y = 0, 0
+        if self.dx >= self.bw:
+            self.dx = 0
+            move_x = self.bw
+        if self.dy >= self.bh:
+            self.dy = 0
+            move_y = self.bh
+        return move_x, move_y
+
+    def move(self, dx, dy):
+        x, y = self.accumulate_delta(dx, dy)
+        self.canvas.move(x, y)
+
+    def update(self):
+        pass
+
+    def print_board(self):
+        for lst in self.board:
+            for n in lst:
+                print(n, end=' ')
+            print()
 
 
 
