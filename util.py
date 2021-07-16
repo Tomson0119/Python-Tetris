bbedb8274b4637b90eac9113701fff45a8e1f106
Tomson0119 <kjs980119@gmail.com>
2021-07-16 13:34:52 +0900

    delta 값을 누적시키는 함수를 따로 뺌

--- util.py
+++ util.py
@@ -1,3 +1,14 @@
+def accumulate_delta(delta, max_val, val):
+    move = 0
+    delta += val
+    if val > 0 and delta >= max_val:
+        delta = 0
+        move = 1
+    elif val < 0 and abs(delta) >= max_val:
+        delta = 0
+        move = -1
+    return delta, move
+
 
 def calc_coord(w, h, r, c):
     return w * c, h * r
